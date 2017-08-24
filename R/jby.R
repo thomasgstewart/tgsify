7a65df07f5f91fe711d061a8ef952def5b88f78a
thomasgstewart <thomas.stewart@vanderbilt.edu>
2017-08-24 14:46:55 -0500

    1

---
+++ R/jby.R
@@ -0,0 +1,20 @@
+#' Calculate dt[,j,by] within pipes (\%>\%)
+#' 
+#' Perform a within data.table calculation dt[,j,by] within a dplyr pipe construction.
+#' @param data A data.table object, say dt
+#' @param expr1 The \code{j} expression for dt[,j,by]
+#' @param expr2 The \code{by} expression for dt[,j,by]
+#' @details Allows dt[,j,by] calculations withing a pipe chain, such as
+#' 
+#' iris \%>\% as.data.table \%>\% jby(mean(Sepal.Length),Species) \%>\% print
+#' 
+#' @keywords jby
+#' @export
+#' @examples
+#' require(data.table); require(dplyr);
+#' iris %>% as.data.table %>% jby(mean(Sepal.Length),Species) %>% print
+
+
+jby <- function(data,expr1,expr2){
+  invisible(data[,eval(substitute(expr1)),eval(deparse(substitute(expr2)))])
+}
