65156a83652f3cd128be4144609f77b13cd4195f
thomasgstewart <thomas.stewart@vanderbilt.edu>
2017-08-24 16:14:17 -0500

    1

--- R/jby.R
+++ R/jby.R
@@ -14,12 +14,25 @@
 #' require(data.table); require(dplyr);
 #' iris %>% as.data.table %>% jby(mean(Sepal.Length),Species) %>% print
 
-jby <- function (data, expr1, expr2) 
-{
-  e1 <- substitute(expr1)
-  e2 <- substitute(expr2)
-  invisible(data[, j = eval(e1), by = eval(deparse(e2))])
+jby <- function(data, expr1, expr2){
+  #browser()
+  com <- "data[, j = " %|% deparse(substitute(expr1)) %|% ", by =" %|% deparse(substitute(expr2)) %|% "]"
+  eval(parse(text = com), envir = getEnvOf("data"))
 }
+
+getEnvOf <- function(what, which=rev(sys.parents())) {
+  for (frame in which)
+    if (exists(what, frame=frame, inherits=FALSE)) 
+      return(sys.frame(frame))
+  return(NULL)
+}
+
+# jby <- function (data, expr1, expr2) 
+# {
+#   e1 <- substitute(expr1)
+#   e2 <- substitute(expr2)
+#   invisible(data[, j = eval(e1), by = eval(deparse(e2))])
+# }
 
 
 # jby <- function(data,expr1,expr2){
