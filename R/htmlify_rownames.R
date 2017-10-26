3d490a7ef17186fb4cae6ac02cf7f324600eb8ec
thomasgstewart <thomas.stewart@vanderbilt.edu>
2017-10-26 18:36:16 -0500

    1

--- R/htmlify_rownames.R
+++ R/htmlify_rownames.R
@@ -1,6 +1,7 @@
 #' Converts hogwash rownames and dimnames to HTML friendly names
 #' 
 #' @param x An array or data.frame
+#' @param ef Numeric.  The expansion factor.  Number of HTML spaces for each leading space.
 #' @details Leading spaces are replaced with \code{&nbsp;} so that the table renders properly in HTML
 #' @md
 #' @keywords htmlify_rownames
@@ -10,16 +11,17 @@
 #' colnames(A) <- c("X1","X2")
 #' htmlify_rownames(A)
 #' @export
-htmlify_rownames <- function(x){
+htmlify_rownames <- function(x, ef = 2){
   UseMethod("htmlify_rownames", x)
 }
 
 
 #' @export
-htmlify_rownames.default <- function(x){
+htmlify_rownames.default <- function(x, ef = 2){
   leadingws <- gsub("^([ ]*)(.*)", "\\1", rownames(x))
   nonleading <- gsub("^([ ]*)(.*)", "\\2", rownames(x))
-  ws <- gsub(" ","&nbsp;", leadingws)
+  htmlspace <- paste(rep("&nbsp;",ef),collapse = "")
+  ws <- gsub(" ", htmlspace, leadingws)
   rownames(x) <- paste0(ws, nonleading)
   return(x)
 }
